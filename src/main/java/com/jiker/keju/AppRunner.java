8e901a8ff017005156a41d3f219cc3b738f52375
Jumpers <elment@msn.cn>
2020-02-19 19:12:05 +0800

    modify main class

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -5,15 +5,24 @@ import java.util.List;
 
 public class AppRunner {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         /*TODO
           1. args[0]为resources下的测试数据文件名，例如传入的args[0]值为"testData.txt"。
           2. 你写的程序将把testDataFile作为参数加载此文件并读取文件内的测试数据，并对每条测试数据计算结果。
           3. 将所有计费结果拼接并使用\n分割，然后保存到receipt变量中。
          */
         String testDataFile = args[0];
-        ParseFile parseFile = new ParseFile(testDataFile);
-        List<TourInfo> tourInfos = parseFile.getTourInfo();
+        ParseFile parseFile = null;
+        List<TourInfo> tourInfos = null;
+        try {
+            parseFile = new ParseFile(testDataFile);
+            tourInfos = parseFile.getTourInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         String receipt = "";
         StringBuffer feeContent = new StringBuffer();
         for (TourInfo tourInfo : tourInfos) {
