c52e15dcd396c9bed2c2ae8b41fdc5be7b431d56
Jumpers <elment@msn.cn>
2020-02-19 18:47:51 +0800

    modify class

--- src/main/java/com/jiker/keju/ParseFile.java
+++ src/main/java/com/jiker/keju/ParseFile.java
@@ -1,8 +1,8 @@
 package com.jiker.keju;
 
-
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -11,30 +11,22 @@ import java.util.regex.Pattern;
 public class ParseFile {
     private final String filePath;
     private ArrayList<String> linesInfo = new ArrayList<String>();
-
-
     public ParseFile(String filePath) throws IOException {
         this.filePath = filePath;
         getLines();
     }
-
-    public List getLines() throws IOException {
-        InputStream rsInputStream = this.getClass().getClassLoader().getResourceAsStream(filePath);
+    public void getLines() throws IOException {
         try {
-//            File file = new File(filePath);
-            BufferedReader bf = new BufferedReader(new InputStreamReader(rsInputStream));
+            BufferedReader bf = new BufferedReader(new InputStreamReader(
+                    this.getClass().getClassLoader().getResourceAsStream(filePath)));
             String tempLine;
-            while ((tempLine = bf.readLine()) != null) {
-                linesInfo.add(tempLine);
-            }
+            while ((tempLine = bf.readLine()) != null) {linesInfo.add(tempLine); }
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
-            rsInputStream.close();
+            this.getClass().getClassLoader().getResourceAsStream(filePath).close();
         }
-        return linesInfo;
     }
-
     public List<TourInfo> getTourInfo() throws Exception {
         List<TourInfo> tourInfos =new ArrayList<TourInfo>();
         for (String oriInfo : linesInfo) {
@@ -43,23 +35,15 @@ public class ParseFile {
         }
         return tourInfos;
     }
-
     private TourInfo parseTxtToObject(String oriInfo) throws Exception {
         TourInfo tourInfo = new TourInfo();
         String RegexExp = "(\\d+)公里,等待(\\d+)分钟";//
-        //get Distance
         Pattern pattern = Pattern.compile(RegexExp);
         Matcher matcher = pattern.matcher(oriInfo);
         if (matcher.find()) {
-            String distance = matcher.group(1);
-            String waitTime = matcher.group(2);
-            tourInfo.setDistance(Integer.parseInt(distance));
-            tourInfo.setWaitTime(Integer.parseInt(waitTime));
-        } else {
-            throw new Exception("无法匹配到行程信息");
-        }
-
+            tourInfo.setDistance(Integer.parseInt(matcher.group(1)));
+            tourInfo.setWaitTime(Integer.parseInt(matcher.group(2)));
+        } else {            throw new Exception("无法匹配到行程信息");        }
         return tourInfo;
-
     }
 }
